592a519a6785d2783a9cdb654e0acc69820aea63
ejanalyst <info@ejanalysis.com>
2015-04-12 18:11:03 -0400

    minor edits

--- R/get.state.info.R
+++ R/get.state.info.R
@@ -19,19 +19,20 @@
 #' Note FIPS were also available here: \cr
 #'  State: \url{http://www.census.gov/geo/reference/ansi_statetables.html}  \cr
 #'  County: \url{http://www.census.gov/geo/www/codes/county/download.html}  \cr
+#' Also see \url{https://www.census.gov/geo/reference/state-area.html} for info on state area and internal point
 #' @param query vector of 1+ elements, which can be \cr
-#'   state FIPS code(s) (as numbers or strings with numbers), \cr 
+#'   state FIPS code(s) (as numbers or strings with numbers), \cr
 #'   state name(s) (exactly matching formats here), or \cr
 #'   2-letter state abbreviation(s) (case insensitive). \cr
-#' @param fields vector of 1+ character string names of the fields available here: 
+#' @param fields vector of 1+ character string names of the fields available here:
 #'   FIPS.ST, ST, statename, ftpname, REGION, is.usa.plus.pr, is.usa, is.state, is.contiguous.us, is.island.areas
 #' @return A data.frame (if query has 2+ elements), providing all or specified fields of information, covering matching states/dc/pr/island areas,
-#'   a vector of the same type of information for just one place (if only 1 query term, i.e., one element in the query vector is provided), 
+#'   a vector of the same type of information for just one place (if only 1 query term, i.e., one element in the query vector is provided),
 #'   or NA if certain problems arise.\cr\cr
 #'   If no query term, or fields not specified, then all information fields are returned: \cr
 #'   FIPS.ST, ST, statename, ftpname, (EPA) REGION, is.usa.plus.pr, is.usa, is.state, is.contiguous.us, is.island.areas
 #' @template seealsoFIPS
-#' @examples 
+#' @examples
 #' #  x <- get.state.info(); str(x); print(''); head(x)
 #' # get.state.info(c('alaska','north carolina', 'montana', "hawaii"), fields=c('ST','statename','REGION'))
 #' # get.state.info('DC'); get.state.info('U.S. Virgin Islands'); get.state.info(4)
@@ -41,95 +42,95 @@
 #' # get.state.info(1:80)
 #' @export
 get.state.info <- function(query, fields='all') {
-  
+
     # ********* INFO IS NOW AVAILABLE VIA data(lookup.states) in proxistat package
   data(lookup.states, package='proxistat')
 
 # used to read in only if not already there but now will read it in again anyway, since had been a bug in code and this ensures using newer fixed version.
 #if (!exists('lookup'))  {
 if (1==0)  {
-    
+
   # SHOULD RECODE PERHAPS TO USE THAT OR AN  OFFICIAL LIST OF STATE NAMES AND FIPS?
   # Otherwise just keep using data below:
-  
+
   # Put the data into (at least local) memory here if not already available:
   # used to use the name lookup but now using lookup.states
   lookup <- structure(list(
-    FIPS.ST = c(NA, "01", "02", "04", "05", "06", 
-"08", "09", "10", "11", "12", "13", "15", "16", "17", "18", "19", 
-"20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", 
-"31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", 
-"42", "44", "45", "46", "47", "48", "49", "50", "51", "53", "54", 
-"55", "56", "60", "66", "69", "72", "74", "78"), ST = c("US", 
-"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", 
-"HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", 
-"MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", 
-"NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", 
-"UT", "VT", "VA", "WA", "WV", "WI", "WY", "AS", "GU", "MP", "PR", 
-"UM", "VI"), 
-    statename = c("United States", "Alabama", "Alaska", 
-"Arizona", "Arkansas", "California", "Colorado", "Connecticut", 
-"Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", 
-"Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", 
-"Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", 
-"Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", 
-"Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", 
-"North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", 
-"Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", 
-"Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", 
-"West Virginia", "Wisconsin", "Wyoming", "American Samoa", "Guam", 
-"Northern Mariana Islands", "Puerto Rico", "U.S. Minor Outlying Islands", 
-"U.S. Virgin Islands"), 
-    ftpname = c("UnitedStates", "Alabama", 
-"Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", 
-"Delaware", "DistrictOfColumbia", "Florida", "Georgia", "Hawaii", 
-"Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", 
-"Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", 
-"Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", 
-"Nevada", "NewHampshire", "NewJersey", "NewMexico", "NewYork", 
-"NorthCarolina", "NorthDakota", "Ohio", "Oklahoma", "Oregon", 
-"Pennsylvania", "RhodeIsland", "SouthCarolina", "SouthDakota", 
-"Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", 
-"WestVirginia", "Wisconsin", "Wyoming", NA, NA, NA, "PuertoRico", 
-NA, NA), 
-    REGION = c(NA, 4, 10, 9, 6, 9, 8, 1, 3, 3, 4, 4, 9, 
-10, 5, 5, 7, 7, 4, 6, 1, 3, 1, 5, 5, 4, 7, 8, 7, 9, 1, 2, 6, 
-2, 4, 8, 5, 6, 10, 3, 1, 4, 8, 4, 6, 8, 1, 3, 10, 3, 5, 8, 9, 
-9, 9, 2, 9, 2), 
-    is.usa.plus.pr = c(FALSE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE), 
-    is.usa = c(FALSE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, 
-FALSE, FALSE, FALSE), 
-    is.state = c(FALSE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE), 
-    is.contiguous.US = c(FALSE, TRUE, FALSE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE), 
-    is.island.areas = c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
-FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
-FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
-FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
-FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
-FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
-FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE)), 
-    .Names = c("FIPS.ST", "ST", "statename", "ftpname", "REGION", 
-  "is.usa.plus.pr", "is.usa", "is.state", "is.contiguous.us", "is.island.areas"), 
+    FIPS.ST = c(NA, "01", "02", "04", "05", "06",
+"08", "09", "10", "11", "12", "13", "15", "16", "17", "18", "19",
+"20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+"31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41",
+"42", "44", "45", "46", "47", "48", "49", "50", "51", "53", "54",
+"55", "56", "60", "66", "69", "72", "74", "78"), ST = c("US",
+"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA",
+"HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA",
+"MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY",
+"NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX",
+"UT", "VT", "VA", "WA", "WV", "WI", "WY", "AS", "GU", "MP", "PR",
+"UM", "VI"),
+    statename = c("United States", "Alabama", "Alaska",
+"Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+"Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii",
+"Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
+"Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan",
+"Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska",
+"Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York",
+"North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon",
+"Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
+"Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
+"West Virginia", "Wisconsin", "Wyoming", "American Samoa", "Guam",
+"Northern Mariana Islands", "Puerto Rico", "U.S. Minor Outlying Islands",
+"U.S. Virgin Islands"),
+    ftpname = c("UnitedStates", "Alabama",
+"Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+"Delaware", "DistrictOfColumbia", "Florida", "Georgia", "Hawaii",
+"Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
+"Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan",
+"Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska",
+"Nevada", "NewHampshire", "NewJersey", "NewMexico", "NewYork",
+"NorthCarolina", "NorthDakota", "Ohio", "Oklahoma", "Oregon",
+"Pennsylvania", "RhodeIsland", "SouthCarolina", "SouthDakota",
+"Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
+"WestVirginia", "Wisconsin", "Wyoming", NA, NA, NA, "PuertoRico",
+NA, NA),
+    REGION = c(NA, 4, 10, 9, 6, 9, 8, 1, 3, 3, 4, 4, 9,
+10, 5, 5, 7, 7, 4, 6, 1, 3, 1, 5, 5, 4, 7, 8, 7, 9, 1, 2, 6,
+2, 4, 8, 5, 6, 10, 3, 1, 4, 8, 4, 6, 8, 1, 3, 10, 3, 5, 8, 9,
+9, 9, 2, 9, 2),
+    is.usa.plus.pr = c(FALSE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE),
+    is.usa = c(FALSE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE,
+FALSE, FALSE, FALSE),
+    is.state = c(FALSE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE),
+    is.contiguous.US = c(FALSE, TRUE, FALSE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE),
+    is.island.areas = c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,
+FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,
+FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,
+FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,
+FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,
+FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,
+FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE)),
+    .Names = c("FIPS.ST", "ST", "statename", "ftpname", "REGION",
+  "is.usa.plus.pr", "is.usa", "is.state", "is.contiguous.us", "is.island.areas"),
     class = "data.frame", row.names = c(NA, 58L))
 }
 
@@ -140,7 +141,7 @@ FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE)),
 
   # If any bad fieldnames are specified, stop.
   if (any(!(fields %in% names(lookup.states)))) {
-    cat('Available field names:\n'); cat(names(lookup.states)); cat('\n'); 
+    cat('Available field names:\n'); cat(names(lookup.states)); cat('\n');
     stop('fields requested must all be among fields available')
   }
 
@@ -155,7 +156,7 @@ FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE)),
 x <- query
 
 # If FIPS.ST is kept at NA, it will match the NA FIPS code that is associated with USA overall in the lookup.states table, so set it to zero.
-x[is.na(x)]  <- 0 
+x[is.na(x)]  <- 0
 
 # prepopulate the output variable
 results <- matrix(NA, nrow=length(x), ncol=length(fields))
@@ -166,11 +167,11 @@ names(results) <- fields
 x <- gsub('^\\s+|\\s+$', '', x)
 
 # FIND WHICH OF QUERY TERMS ARE VALID FIPS.ST AND GET STATE DATA FOR THOSE
-is.valid.FIPS.ST <- grepl('^[0-9]*$', x) 
+is.valid.FIPS.ST <- grepl('^[0-9]*$', x)
 is.valid.FIPS.ST[is.valid.FIPS.ST] <- as.numeric(x[is.valid.FIPS.ST]) %in% as.numeric(lookup.states$FIPS.ST)
 # cbind(x, is.valid.FIPS.ST  )
 # as.numeric('NY') would fail
-results[is.valid.FIPS.ST, ] <- lookup.states[ match(as.numeric(x[is.valid.FIPS.ST]), as.numeric(lookup.states$FIPS.ST)), fields] 
+results[is.valid.FIPS.ST, ] <- lookup.states[ match(as.numeric(x[is.valid.FIPS.ST]), as.numeric(lookup.states$FIPS.ST)), fields]
 
 # FIND WHICH OF QUERY TERMS ARE VALID statename AND GET STATE DATA FOR THOSE
 upx <- toupper(x)
